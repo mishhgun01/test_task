9d6b844910cd2e5f6350f988f40a5df312172f8f
mishhgun01 <AVATARno15>
2022-09-30 21:37:54 +0300

    current keys

--- pkg/storage.go
+++ pkg/storage.go
@@ -28,7 +28,9 @@ func NewStorage(conn string, dur time.Duration) *Storage {
 func (s *Storage) GetData() (GasStatistics, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	data, err := s.client.Get(strconv.Itoa(time.Now().Year()) + "/" + time.Now().Month().String() + "/" + strconv.Itoa(time.Now().Day()) + "/" + strconv.Itoa(time.Now().Hour())).Result()
+	// ключ в формате год/месяц/денб/час
+	key := strconv.Itoa(time.Now().Year()) + "/" + time.Now().Month().String() + "/" + strconv.Itoa(time.Now().Day()) + "/" + strconv.Itoa(time.Now().Hour())
+	data, err := s.client.Get(key).Result()
 	if err != nil {
 		return GasStatistics{}, err
 	}
@@ -45,7 +47,9 @@ func (s *Storage) UpdateData(data GasStatistics) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	jsonData, err := json.Marshal(data)
-	s.client.Set(strconv.Itoa(time.Now().Year())+"/"+time.Now().Month().String()+"/"+strconv.Itoa(time.Now().Day())+"/"+strconv.Itoa(time.Now().Hour()), jsonData, s.dur)
+	// ключ в формате год/месяц/денб/час
+	key := strconv.Itoa(time.Now().Year()) + "/" + time.Now().Month().String() + "/" + strconv.Itoa(time.Now().Day()) + "/" + strconv.Itoa(time.Now().Hour())
+	s.client.Set(key, jsonData, s.dur)
 	return err
 }
 
