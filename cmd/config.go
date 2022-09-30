6e436cd8c543b4b511d23fde898631751dce2276
mishhgun01 <AVATARno15>
2022-09-30 21:07:25 +0300

    config.go

---
+++ cmd/config.go
@@ -0,0 +1,4 @@
+package main
+
+const ADDR = "localhost:8080"
+const CONN = "127.0.0.1:6379"
--- cmd/main.go
+++ cmd/main.go
@@ -8,10 +8,10 @@ import (
 )
 
 func main() {
-	s := pkg.NewStorage("127.0.0.1:6379", time.Hour)
+	s := pkg.NewStorage(CONN, time.Hour)
 	api := pkg.New(mux.NewRouter(), s)
 	api.Handle()
-	err := api.ListenAndServe("localhost:8080")
+	err := api.ListenAndServe(ADDR)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
